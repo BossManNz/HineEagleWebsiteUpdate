21177299906ea83cfb594897a2e6500ed4a594d8
Boss <33207655+BossManNz@users.noreply.github.com>
2025-11-08 11:53:53 +1300

    Initial Commit

---
+++ assets/js/reviews-data.js
@@ -0,0 +1,19 @@
+/* Hine Eagle Reviews, simple data-only file */
+window.HE_REVIEWS = [
+  {
+    text: "Best lawyers in Tauranga. I've been in and out of so many law firms trying to sort my separation and custody of my son. The entire team at Hine Eagle has been great to deal with.",
+    client: "Client, M.B"
+  },
+  {
+    text: "I couldn’t recommend nicer, more caring, hardworking lawyers than Hine Eagle. They truly are the best at what they do.",
+    client: "Client, K.T"
+  },
+  {
+    text: "I have had the best experience with Hine Eagle. Highly recommend 👌",
+    client: "Client, S.B"
+  },
+  {
+    text: "Professional, caring, and all about justice for the client and child. I would give 10 stars if I could. Truly grateful for all the hard work and effort put into my case and getting the best outcome possible.",
+    client: "Client, M"
+  },
+];
